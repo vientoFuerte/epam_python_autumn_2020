262798ba16026aff7234fe9bb21a6d3a2d607a42
vientoFuerte <49725325+vientoFuerte@users.noreply.github.com>
2021-02-28 02:54:02 +0300

    Лёд тронулся

--- lecture_01/hw/task02.py
+++ lecture_01/hw/task02.py
@@ -10,4 +10,24 @@ from collections import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    ...
+    i = None
+    j = None
+    for k in data:
+        if i is None:
+            i = k
+        elif j is None:
+            j = k
+        else:
+            i, j = j, i + j
+            if k != j:
+                return False
+    return True
+
+
+
+
+def test_check_fibonacci():
+    assert check_fibonacci([0, 1, 1, 2, 3, 5, 8, 13]) is True
+
+def test_check_fibonacci_fail():
+    assert check_fibonacci([0, 1, 1, 2, 3, 5, 8, 133]) is False
